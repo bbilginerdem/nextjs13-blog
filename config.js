2ff4c23a45bfc48ac8aef881d006abd6e31db162
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2023-09-03 20:12:27 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-DB_URI =
-	'mongodb+srv://dbBlog:X8oeuIGA6YTxxjHA@cluster1111.xd3xti6.mongodb.net/?retryWrites=true&w=majority'
-API = 'http://localhost:3000/api'
-NEXTAUTH_SECRET= 'dijfsodfij)#$@(@#)92834'
-
-module.exports = {
-	DB_URI,
-	API,
-}
