0fbf2e2b90a978363d7fb245fd1bdf8ff1adbc9a
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2023-09-13 00:51:51 +0300

    register login done

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,20 +1,28 @@
 import Link from 'next/link'
+import { useSession, signOut } from 'next-auth/react'
 
 export default function TopNav() {
+	const { data, status } = useSession()
+	console.log('data=> ', data)
+	console.log('status=> ', status)
 	return (
 		<nav className="nav shadow p-2 justify-content-between mb-3">
 			<Link className="nav-link" href="/">
 				BLOG
 			</Link>
 
-			<div className="d-flex">
-				<Link className="nav-link" href="/login">
-					Login
-				</Link>
-				<Link className="nav-link" href="/register">
-					Register
-				</Link>
-			</div>
+			{status === 'authenticated' ? (
+				<></>
+			) : (
+				<div className="d-flex">
+					<Link className="nav-link" href="/login">
+						Login
+					</Link>
+					<Link className="nav-link" href="/register">
+						Register
+					</Link>
+				</div>
+			)}
 		</nav>
 	)
 }
