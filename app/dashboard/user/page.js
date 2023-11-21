0e51c0e22e12732469af6b92612fc05a3ffcb3d0
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2023-11-21 20:59:33 +0300

    dashboard and logout added for when user signed in
    and google sign in added

---
+++ app/dashboard/user/page.js
@@ -0,0 +1,12 @@
+export default function UserDashboard() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <p>Dashboard</p>
+          <p>...</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/TopNav.js
+++ components/TopNav.js
@@ -2,26 +2,37 @@ import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
 
 export default function TopNav() {
-	const { data, status } = useSession()
+  const { data, status } = useSession()
+  console.log("🚀 ~ file: TopNav.js:6 ~ TopNav ~  data, status :", data, status)
 
-	return (
-		<nav className="nav shadow p-2 justify-content-between mb-3">
-			<Link className="nav-link" href="/">
-				BLOG
-			</Link>
 
-			{status === 'authenticated' ? (
-				<></>
-			) : (
-				<div className="d-flex">
-					<Link className="nav-link" href="/login">
-						Login
-					</Link>
-					<Link className="nav-link" href="/register">
-						Register
-					</Link>
-				</div>
-			)}
-		</nav>
-	)
+  return (
+    <nav className="nav shadow p-2 justify-content-between mb-3">
+      <Link className="nav-link" href="/">
+        BLOG
+      </Link>
+
+      {status === "authenticated" ? (
+        <>
+          <div className="d-flex">
+            <Link className="nav-link" href="/dashboard/user">
+              Dashboard
+            </Link>
+            <a className="nav-link pointer" onClick={() => signOut({ callbackUrl: '/login' })}>
+              Logout
+            </a>
+          </div>
+        </>
+      ) : (
+        <div className="d-flex">
+          <Link className="nav-link" href="/login">
+            Login
+          </Link>
+          <Link className="nav-link" href="/register">
+            Register
+          </Link>
+        </div>
+      )}
+    </nav>
+  )
 }
