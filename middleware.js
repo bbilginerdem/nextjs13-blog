ad9f806a17bbe0114578fec4389f0d4002aa902b
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2023-11-23 03:31:08 +0300

    dashboard for admin added and user restricted from accessing it

--- middleware.js
+++ middleware.js
@@ -1,3 +1,32 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export const config = {matcher: ["/dashboard/:path*"]}
+export const config = {
+  matcher: [
+    '/dashboard/user/:path*',
+    '/dashboard/admin/:path*',
+    '/api/user/:path*',
+    '/api/admin/:path*',
+  ],
+}
+
+// role based authorization
+
+export default withAuth(
+  async function middleware (req) {
+    const url = req.nextUrl.pathname
+    const userRole = req?.nextauth?.token?.user?.role
+    if(url?.includes('/admin') && userRole !== 'admin') {
+      return NextResponse.redirect(new URL('/',req.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        if(!token) {
+          return false
+        }
+      },
+    },
+  },
+)
